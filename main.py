af328cf162d7f5e3651c18f81fdda05394f54eb0
Reni-Rena <81166304+Reni-Rena@users.noreply.github.com>
2026-03-07 00:11:46 +0100

    image + file

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 from preprocessing import ETHDataset
 from utils import world_to_image
 
 # path vers le dataset
-data_path = "biwi_eth.txt"
+data_path = "Eth_Data.txt"
 dataset = ETHDataset(data_path)
 print("Nombre de séquences :", len(dataset))
 sample = dataset[0]
@@ -21,13 +22,14 @@ plt.show()
 
 # A changer avec les .txt homographique en fonction de la scène
 # np.loadtxt("eth.txt")
-H = np.eye(3)
+H = np.loadtxt("Eth_Homo.txt")
 
 obs_img = world_to_image(obs, H)
 pred_img = world_to_image(pred, H)
 
-# image noir pour le dessins
-scene_image = np.zeros((600,800,3))
+# Charger l'image de fond de la scène
+scene_image = cv2.imread("Eth_Bg.png")
+scene_image = cv2.cvtColor(scene_image, cv2.COLOR_BGR2RGB)
 
 plt.imshow(scene_image)
 plt.plot(obs_img[:,0], obs_img[:,1], 'bo-')
